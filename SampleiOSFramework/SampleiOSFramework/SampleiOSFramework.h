d4a37767aeb6f82246563564f84fb4f026155eda
zac24 <dwi.pra24@gmail.com>
2020-10-20 15:59:07 +0530

    created sample framework

---
+++ SampleiOSFramework/SampleiOSFramework/SampleiOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleiOSFramework.h
+//  SampleiOSFramework
+//
+//  Created by Prashant Dwivedi on 20/10/20.
+//  Copyright © 2020 Prashant Dwivedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleiOSFramework.
+FOUNDATION_EXPORT double SampleiOSFrameworkVersionNumber;
+
+//! Project version string for SampleiOSFramework.
+FOUNDATION_EXPORT const unsigned char SampleiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleiOSFramework/PublicHeader.h>
+
+
---
+++ SampleiOSFramework/SampleiOSFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  SampleiOSFramework
+//
+//  Created by Prashant Dwivedi on 20/10/20.
+//  Copyright © 2020 Prashant Dwivedi. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init () {
+        
+    }
+    
+    public static func someMethod () -> String {
+        return "This method has been called from Sample iOS framework"
+    }
+}
+
