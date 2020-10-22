1eb2c335dc7863d0e2dd949d36d1c3cd76e950b1
zac24 <dwi.pra24@gmail.com>
2020-10-22 17:50:58 +0530

    changes

--- SampleiOSFramework/Service.swift
+++ SampleiOSFramework/Service.swift
@@ -9,46 +9,46 @@
 import UIKit
 import Foundation
 
-public class Service: UIView {
+public class Service: UIViewController {
     @IBOutlet var backgroundView: UIView!
     
-    let nibName = "ServiceView"
-    var contentView: UIView!
+//    let nibName = "ServiceView"
+//    var contentView: UIView!
 //    private init () {
 //
 //    }
-    public override init(frame: CGRect) {
-      super.init(frame: frame)
-      setUpView()
-    }
+//    public override init(frame: CGRect) {
+//      super.init(frame: frame)
+//      setUpView()
+//    }
 
     public required init?(coder aDecoder: NSCoder) {
        // For use in Interface Builder
        super.init(coder: aDecoder)
-      setUpView()
+//      setUpView()
     }
     
-    private func setUpView() {
-        let bundle = Bundle(for: type(of: self))
-        let nib = UINib(nibName: self.nibName, bundle: bundle)
-        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
-        addSubview(contentView)
-
-        contentView.center = self.center
-        contentView.autoresizingMask = []
-        contentView.translatesAutoresizingMaskIntoConstraints = true
-    }
-    
-//    public static func presentFirstViewControllerOn(_ viewController:UIViewController) {
-//        let storyBoard : UIStoryboard = UIStoryboard(name: "Storyboard", bundle:nil)
-//        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "Service")
-//        viewController.present(nextViewController, animated:true, completion:nil)
+//    private func setUpView() {
+//        let bundle = Bundle(for: type(of: self))
+//        let nib = UINib(nibName: self.nibName, bundle: bundle)
+//        self.contentView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+//        addSubview(contentView)
+//
+//        contentView.center = self.center
+//        contentView.autoresizingMask = []
+//        contentView.translatesAutoresizingMaskIntoConstraints = true
 //    }
+    
+    public static func presentFirstViewControllerOn(_ viewController:UIViewController) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Storyboard", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "Service") as! viewController
+        viewController.present(nextViewController, animated:true, completion:nil)
+    }
     
     
     public static func someMethod () -> String {
         
-        return "It's a new Message 3"
+        return "It's a new Message 4"
         
     }
 }
